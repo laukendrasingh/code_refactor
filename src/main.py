5893742a4b0526b9b8a6de15c3d8996e7c2a97f7
laukendra <laukendras@gmail.com>
2024-06-24 08:45:15 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,5 +5,6 @@ print("Start: code refactor")
 render_ui()
 print("End: code refactor")
 
+#IT'S NOT USING IN POC. ADDED ONLY FOR LEARNING.
 if __name__ == '__main__':
     app.run(debug=True, port=50010, use_reloader=False)
